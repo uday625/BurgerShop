a162e3e7641aa0e0b89f5c8b0328cac372f734f5
UDAY BAG <uday625@Nemesis-2.local>
2018-11-18 16:39:03 -0500

    added total price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,11 +14,14 @@ const controls = [
 const BuildControls = (props) => {
     return(
         <div className = "BuildControls"> 
+            <p>Current price: <strong>${props.price.toFixed(2)}</strong></p>
             {controls.map(ctrl => (
                     <BuildControl 
                         key ={ctrl.label} 
                         label ={ctrl.label}
                         added = {() => props.ingredientAdded (ctrl.type)}
+                        removed ={ () => props.ingredientRemoved (ctrl.type)}
+                        disabled = {props.disabled[ctrl.type]}
                         />
                 ))}        
         </div>
